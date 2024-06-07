1621d7e0af6e88f97943891faf1b0955fb2d7761
Minmgf <manuelinm2015@gmail.com>
2024-06-07 13:13:52 -0500

    icons fixed

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import gitHubIcon from "../assets/icons/github.svg"
+import linkedInIcon from "../assets/icons/linkedin.svg"
+import igIcon from "../assets/icons/ig.svg"
 
 const SocialNetworks = ({ igLink, linkedinLink, gitLink }) => {
     return (
         <div className='flex justify-center gap-4'>
             <a href={igLink}  target="blank" className='hover:scale-125 hover:transition-all'>
-                <img src="../assets/icons/ig.svg" className='w-8 h-8' alt="" />
+                <img src={igIcon} className='w-8 h-8' alt="" />
             </a>
             {/* <a href={linkedinLink} target="blank"  className=''>
-                <img src="../assets/icons/linkedin.svg" className='w-8 h-8' alt="" />
+                <img src={linkedInIcon} className='w-8 h-8' alt="" />
             </a> */}
             <a href={gitLink} target="blank"  className='hover:scale-125 hover:transition-all'>
-                <img src="../assets/icons/github.svg" className='w-8 h-8' alt="" />
+                <img src={gitHubIcon} className='w-8 h-8' alt="" />
             </a>
         </div>
     )
